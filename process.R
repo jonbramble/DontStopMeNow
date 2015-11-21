ceb8c01da7ad8003f71d639bd8920b84c7978d09
Jonathan Bramble <phyjpb@leeds.ac.uk>
2015-11-21 20:09:23 +0000

    Added a ribbon bar

--- process.R
+++ process.R
@@ -4,12 +4,14 @@ library(plyr)
 base_dir = "/media/jon/kelima/jon/Data/StoppedFlow/Durham_140515"
 data.sources = list.files(base_dir,pattern="*.csv",full.names=TRUE)
 
+#function to read in the data files, ignoring the first 34 rows of csv file
+#adds the buffer, lipid, temp and conc from the file names only
 annotate <- function(f){
   dat <- read.table(f,skip=34,nrows=1000,sep=",")
   #read in more information for dataframe
   name <- basename(f)
   name_list <- unlist(strsplit(name,"['.',' ']")[[1]])
-  print(name_list)
+  #print(name_list)
   dat$buffer <- as.factor(name_list[1])
   dat$lipid <- as.factor(name_list[2])
   dat$temp <- as.factor(name_list[3])
@@ -17,12 +19,9 @@ annotate <- function(f){
   return(dat)
 }
 
+#the trace function grouped by concentration
 plot_trace <- function(df,abuffer,alipid,atemp){
-  print(abuffer)
-  print(alipid)
-  print(atemp)
   plot_data <- subset(df, buffer == abuffer & lipid == alipid & temp == atemp)
-  print(nrow(plot_data))
   buffer_str <- paste("Buffer:",abuffer)
   lipid_str <- paste("Lipid:", alipid)
   temp_str <- paste("Temp:", atemp, "C")
@@ -34,11 +33,25 @@ plot_trace <- function(df,abuffer,alipid,atemp){
 yb <- ldply(data.sources,annotate)
 #average the repeats
 yb$avg <- apply(yb[2:7],1,mean)
-colnames(yb) <- c("Time","R0","R1","R2","R3","R4","R5","buffer","lipid","temp","conc","Avg")
+#calc the sd of the repeats
+yb$sd <- apply(yb[2:7],1,sd)
+#here we correct the colnames
+colnames(yb) <- c("Time","R0","R1","R2","R3","R4","R5","buffer","lipid","temp","conc","Avg","sd")
 
 
 p <- plot_trace(yb,"PBS","POPCPOPS",37)
 p
 
-tset <- subset(yb, buffer == "PBS" & lipid == "POPCPOPS" & temp == 25 & conc==25 )
-plot(tset$Time,tset$R0)
+syb <- subset(yb, buffer == "PBS" & lipid == "POPCPOPS" & temp == 45)
+r <- ggplot( syb, aes(x=Time,y=Avg, group=conc, color=conc)) + geom_line() + theme_minimal(base_size=22) + xlab("Time (s)") + ylab("Var") + scale_color_discrete(name="Concentration")
+r + geom_ribbon(aes(ymin=Avg-sd,ymax=Avg+sd),alpha=0.1)
+
+
+m <- melt(yb,id.vars=c("Time","buffer","lipid","temp","conc","Avg","Var"), variable.name="R",value.name="DF")
+
+
+
+
+tset <- subset(m, buffer == "PBS" & lipid == "POPCPOPS" & temp == 25 & conc==12 )
+q <- ggplot(tset,aes(x=Time,y=DF,group=R,color=R))
+q + geom_line() + theme_minimal(base_size=18)
