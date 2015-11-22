395adc096e17cd7bfff0bfc87c8ee0554f3bb77a
Jonathan Bramble <phyjpb@leeds.ac.uk>
2015-11-22 15:01:41 +0000

    added simple fit

--- process.R
+++ process.R
@@ -1,5 +1,7 @@
 library(ggplot2)
 library(plyr)
+library(reshape2)
+library(minpack.lm)
 
 base_dir = "/media/jon/kelima/jon/Data/StoppedFlow/Durham_140515"
 data.sources = list.files(base_dir,pattern="*.csv",full.names=TRUE)
@@ -39,7 +41,7 @@ yb$sd <- apply(yb[2:7],1,sd)
 colnames(yb) <- c("Time","R0","R1","R2","R3","R4","R5","buffer","lipid","temp","conc","Avg","sd")
 
 #To inspect the individual repeats need a melt version
-myb <- melt(yb,id.vars=c("Time","buffer","lipid","temp","conc","Avg","Var"), variable.name="R",value.name="DF")
+myb <- melt(yb,id.vars=c("Time","buffer","lipid","temp","conc","Avg","sd"), variable.name="R",value.name="DF")
 
 #use the plot_function
 p <- plot_trace(yb,"PBS","POPCPOPS",37)
@@ -51,6 +53,45 @@ r <- ggplot( syb, aes(x=Time,y=Avg, group=conc, color=conc)) + geom_line() + the
 r + geom_ribbon(aes(ymin=Avg-sd,ymax=Avg+sd),alpha=0.1)
 
 #look at repeats in detail
-tset <- subset(myb, buffer == "PBS" & lipid == "POPCPOPS" & temp == 25 & conc==12 )
+tset <- subset(myb, buffer == "PBS" & lipid == "POPCPOPS" & temp == 25 & conc==100 )
 q <- ggplot(tset,aes(x=Time,y=DF,group=R,color=R))
-q + geom_line() + theme_minimal(base_size=18)
+q + geom_point() + theme_minimal(base_size=18)
+
+
+
+#simple fitting
+
+#define a suitable test function
+exp1 <- function(tt,params){
+  params$B + (params$A-params$B)*(1-exp(-1*params$k1*tt))
+}
+
+residFun <- function(p,observed,tt){
+  observed - exp1(tt,p)
+}
+
+t=seq(0,60,length.out=1000)
+
+pp = list(A=9,B=8,k1=0.05)
+simDNoisy <- exp1(t,pp) #+ rnorm(1000,sd=.01)
+plot(t,simDNoisy,type="l")
+#rs <- residFun(list(A=10,k1=-0.01),simDNoisy,t)
+
+parStart <- list(A=9,B=8,k1=0.01)
+
+obs <- subset(tset, R=="R5")
+
+nls.out <- nls.lm(par=parStart, fn = residFun, observed = obs$DF, tt = t, control = nls.lm.control(nprint=1))
+summary(nls.out)
+
+fitLine <- sapply(t,exp1,nls.out$par)
+
+layout(matrix(1:2, ncol = 1), widths = 1, heights = c(2,1), respect = FALSE)
+par(mar = c(0, 4.1, 4.1, 2.1))
+plot(obs$Time,obs$DF,xaxt = 'n')
+lines(t,fitLine)
+par(mar = c(4.1, 4.1, 0, 2.1))
+plot(t,nls.out$fvec)
+
+
+
